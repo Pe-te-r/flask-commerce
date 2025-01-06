6968f2989be90326b1c7661ae35550d5c20a4e68
Pe-te-r <mburupeter8526@gmail.com>
2025-01-06 19:48:54 +0300

    configuration setup done

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_migrate import Migrate
 from app.routes import register_routes
 from app.database import db
+from config import Config
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
     
     db.init_app(app)
+    Migrate(app,db)
     register_routes(app)
     
     return app
---
+++ config.py
@@ -0,0 +1,17 @@
+from os import environ
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL")
+    SECRET_KEY = environ.get("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = "sandbox.smtp.mailtrap.io"
+    MAIL_PORT = 2525
+    MAIL_USERNAME = "a040f4f871f7b5"
+    MAIL_PASSWORD = "634e1d8d2d465e"
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
