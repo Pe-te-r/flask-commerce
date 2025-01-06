0e80ca29f925b560947c8bc658ca2768028a7ed8
Pe-te-r <mburupeter8526@gmail.com>
2025-01-07 01:08:29 +0300

    register and login

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,18 +1,11 @@
 from flask import request, jsonify, Blueprint
-from app.database.models import User,db,Password
+from app.database.models import User,db
 
 user_bp =Blueprint('user_bp',__name__)
 
 @user_bp.route('/users',methods=['GET'])
 def get_users():
-    # user = User(first_name='peter',last_name='wahu',email='phantom8526@duck.go')
-    # print(user)
-    # db.session.add(user)
-    # db.session.commit()
-    print('here 1')
     users= User.query.all()
-    print('here 2')
-    print(users)
     return jsonify({'data':[user.to_json() for user in users]})
 
 @user_bp.route('/users',methods=['POST'])
@@ -21,6 +14,10 @@ def register_user():
 
     if 'email' not in data or  'password' not  in data or 'last_name' not in data or 'first_name' not in data:
         return jsonify({'error':'some information missing'})
+    
+    if User.get_by_email(data['email']):
+        return jsonify({'error':'email already exists'}),409
+
     password = data['password']
     del data['password']
 
@@ -28,17 +25,21 @@ def register_user():
     db.session.add(new_user)
     db.session.commit()
     new_user.save_password(password)
-    return jsonify({'data':data})
+    return jsonify({'data':data}),201
 
 @user_bp.route('/login',methods=['POST'])
 def login():
     data = request.get_json()
-    # user = User.query.filter_by(email=data['email']).first
     user = User.get_by_email(email=data['email'])
     if not user:
-        return jsonify({'error':'Email not found'})
-    # password = Password.query.filter_by(user_id=user.id)
+        return jsonify({'error':'Email not found'}),404
     if user.verify_password(data["password"]):
+        return jsonify({'data':user.to_json()}),200
+        
+    return jsonify({'error':'an error'}),
 
-        return jsonify({'data':user.to_json()})
-    return jsonify({'error':'an error'})
+@user_bp.route('/users',methods=['DELETE'])
+def delete_user():
+    data = request.get_json()
+    user = User.get_by_email(data['email'])
+    return user
