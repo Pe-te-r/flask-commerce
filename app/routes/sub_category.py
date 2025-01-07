fc54ca5911c0177f7816df7e4f29cfd046181279
Pe-te-r <mburupeter8526@gmail.com>
2025-01-07 23:27:30 +0300

    subcategory better route

--- app/routes/sub_category.py
+++ app/routes/sub_category.py
@@ -1,42 +1,46 @@
 from flask import  jsonify, request, Blueprint
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required,get_jwt
 from uuid import uuid4
 from app.database.models import SubCategory,db,Category
 
 sub_category_route  = Blueprint('sub_category_route',__name__)
 
-# create sub_category and get all sub_category
-@sub_category_route.route('/subcategory',methods=['GET','POST'])
-@jwt_required()
-def sub_category():
-    if request.method =="GET":
+@sub_category_route.route('/subcategory',methods=['GET'])
+def get_sub_category():
         subcategorys = SubCategory.query.all()
         if not subcategorys:
             return jsonify({'error':'no sub category available'})
         
         return jsonify({'data':[subcategory.to_json() for subcategory in subcategorys]})
-    
-    if request.method == 'POST':
-        data  = request.get_json()
-        if not data['category'] or not data['name']:
-            return jsonify({'error':'missing important info'})
-        category = Category.get_category_by_name(data["category"])
-        if not category:
-            return jsonify({'error':'category not available'})
-        sub_category_exits=SubCategory.get_sub_category_by_name(data['name'])
-        if sub_category_exits:
-            return jsonify({'error':'sub category already available'})
-        subcategory = SubCategory(id=uuid4(),category_id=category.id,name=data['name'])
-        db.session.add(subcategory)
-        db.session.commit()
 
-        return jsonify({'data':subcategory.to_json()})
+# create sub_category and get all sub_category
+@sub_category_route.route('/subcategory',methods=['GET','POST'])
+@jwt_required()
+def sub_category():
+    claims = get_jwt()
+    if claims.get('role') != 'admin':
+        return jsonify({'error':'this action is not authorized'})
+    data  = request.get_json()
+    if not data['category'] or not data['name']:
+         return jsonify({'error':'missing important info'})
+    category = Category.get_category_by_name(data["category"])
+    if not category:
+         return jsonify({'error':'category not available'})
+    sub_category_exits=SubCategory.get_sub_category_by_name(data['name'])
+    if sub_category_exits:
+         return jsonify({'error':'sub category already available'})
+    subcategory = SubCategory(id=uuid4(),category_id=category.id,name=data['name'])
+    db.session.add(subcategory)
+    db.session.commit( )
+    return jsonify({'data':subcategory.to_json()})
 
 # update sub_category
 @sub_category_route.route('/subcategory/<string:subcategory>',methods = ['PUT'])
 @jwt_required()
 def update_sub_category(subcategory):
-    print('update')
+    claims = get_jwt()
+    if claims.get('role') != 'admin':
+        return jsonify({'error':'you cannot perfom this action'})
     sub_category = SubCategory.get_sub_category_by_name(subcategory)
 
     if not sub_category:
@@ -64,7 +68,9 @@ def update_sub_category(subcategory):
 @sub_category_route.route('/subcategory/<string:subcategory>',methods = ['DELETE'])
 @jwt_required()
 def delete_sub_category(subcategory):
-    print('delete')
+    claims = get_jwt()
+    if claims.get('role') !='admin':
+        return jsonify({'error':'you cannot perform this action'})
     sub_category = SubCategory.get_sub_category_by_name(subcategory)
     if not sub_category:
         return jsonify({'error':'sub category does not exits'}),404
@@ -76,7 +82,6 @@ def delete_sub_category(subcategory):
 # get one sub_category
 @sub_category_route.route('/subcategory/<string:subcategory>',methods =['GET'])
 def get_one_sub_category(subcategory):
-    print(subcategory)
     sub_category = SubCategory.get_sub_category_by_name(subcategory)
     if not sub_category:
         return jsonify({'error':'sub category does not exists'})
