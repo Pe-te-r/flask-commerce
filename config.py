35a1574f27737d545093ea8b86fef9346dbcd1a1
Pe-te-r <mburupeter8526@gmail.com>
2025-01-16 22:05:49 +0300

    better changes

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ class Config:
     SECRET_KEY = environ.get("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = environ.get('MAIL_SERVER')
-    JWT_ACCESS_TOKEN_EXPIRES=timedelta(days=environ.get('TIME'))
+    JWT_ACCESS_TOKEN_EXPIRES=timedelta(days=int(environ.get('TIME')))
     MAIL_PORT = environ.get("PORT")
     MAIL_USERNAME = environ.get("USER_MAIL") 
-    MAIL_PASSWORD =environ("USER_PASSWORD")
+    MAIL_PASSWORD =environ.get("USER_PASSWORD")
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
