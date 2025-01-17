1234db32e93a29e637f9e767c56272e38af471d1
Pe-te-r <mburupeter8526@gmail.com>
2025-01-17 20:30:13 +0300

    account info

--- app/routes/users.py
+++ app/routes/users.py
@@ -7,52 +7,27 @@ from app.database.models import User,db,Role_Enum
 user_bp =Blueprint('user_bp',__name__)
 
 # get one user
-# @user_bp.route("/users", methods=["GET"])
-# @jwt_required()
-# def get_user(email):
-#     email = request.args.get('email')
-#     id = request.args.get('id')
+@user_bp.route("/users/<string:id>", methods=["GET"])
+@jwt_required()
+def get_user(id):
+    print(id)
+    user = User.get_by_id(UUID(id))
+    if not user:
+        return jsonify({"error": "user not found"}), 409
 
-#     user=None
-#     if email:
-        # user = User.get_by_email(email)
-#     if id:
-#         user = User.get_by_id(id)
-#     if not user:
-#         return jsonify({"error": "user not found"}), 409
-
-#     return jsonify(user.to_json())
+    return jsonify(user.to_json())
 
 # get all users
 @user_bp.route('/users',methods=['GET'])
 @jwt_required()
 def get_users():
     # params
-    email = request.args.get('email')
     orders = request.args.get('orders')
-    id = request.args.get('id')
-    # auth
-    token_mail = get_jwt_identity()
+
     claims = get_jwt()
     role = claims.get('role')
-    user = None
-    if email:
-        user = User.get_by_email(email)
-    if id:
-        user = User.get_by_id(UUID(id))
-    
-    if (email or id) and not user:
-        return jsonify({'error':"no such user info"})
-
-    if user and user.email != token_mail and  role !='admin':
-        return jsonify({'error':'cannot access someone else data'})
-    if user and orders:
-        return jsonify({"data": user.to_json(show_orders=True)})
-    elif user:
-            return jsonify({'data':user.to_json()})
-
     if role !='admin':
-        return jsonify({'error':'user not allowed'})
+        return jsonify({'error':'user not allowed'}),403
 
     users= User.query.all()
     if not users:
